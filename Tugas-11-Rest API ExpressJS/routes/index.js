047f2bef2c2da76bba43db3bc46365490f04765e
roannarum <dewiarumsari38@gmail.com>
2022-03-22 09:24:07 +0700

    Tugas REST API-ExpressJS

---
+++ Tugas-11-Rest API ExpressJS/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const fspromises = require('fs/promises')
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/register',(req, res) =>{
+  try {
+    let {name, role, password} = req.body
+    let obj = {name, role, password, isLogin: false}
+    let existingData = await fspromises.readFile('dataases.json')
+    let realData = JSON.parse(existingData)
+
+    realData.push(obj)
+    console.log(realData)
+    await fspromises.writeFile('data.json', JSON.stringify(realData))
+    res.status(201).json(realData)
+  }catch(error){
+    return res.status(400).json({ message: 'error', error : error})
+  }
+})
+
+module.exports = router;
